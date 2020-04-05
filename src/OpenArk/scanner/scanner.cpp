3a0a18f47f2e70307ae0a5fa2253ccf520e1362f
BlackINT3 <blackint3@gmail.com>
2020-04-05 23:20:06 +0800

    Fix pe scanner bug

--- src/OpenArk/scanner/scanner.cpp
+++ src/OpenArk/scanner/scanner.cpp
@@ -426,7 +426,7 @@ void Scanner::RefreshSummary(const std::wstring& path)
 	DWORD64 size;
 	UNONE::FsGetFileSizeW(path, (DWORD64&)size);
 	kbytes = size / 1024;
-	mbytes = size / 1024 / 1024;
+	mbytes = kbytes / 1024;
 	std::wstring formed = UNONE::StrFormatW(L"%.2f MB | %.2f KB | %d B", mbytes, kbytes, size);
 
 	auto AddSummaryUpItem = [&](QString name, QString value) {
@@ -763,8 +763,14 @@ void Scanner::RefreshExport()
 
 	PIMAGE_DATA_DIRECTORY dir = UNONE::PeGetDataDirectory(IMAGE_DIRECTORY_ENTRY_EXPORT, pe_image_);
 	PDWORD addr_names = (PDWORD)(exp->AddressOfNames + pe_image_);
+	if (!UNONE::PeRegionValid(pe_image_, addr_names))
+		return;
 	PDWORD addr_funcs = (PDWORD)(exp->AddressOfFunctions + pe_image_);
+	if (!UNONE::PeRegionValid(pe_image_, addr_funcs))
+		return;
 	PWORD addr_ordinals = (PWORD)(exp->AddressOfNameOrdinals + pe_image_);
+	if (!UNONE::PeRegionValid(pe_image_, addr_ordinals))
+		return;
 	DWORD cnt_names = exp->NumberOfNames;
 	DWORD cnt_ordinals = exp->NumberOfFunctions;
 	DWORD base_ordinal = exp->Base;
